3a0b58954882dc895653be8e5010e81013fe6ac0
gitttt-1234 <64513125+gitttt-1234@users.noreply.github.com>
2023-03-05 22:06:28 -0800

    Create run_classifier.py

---
+++ run_classifier.py
@@ -0,0 +1,59 @@
+import os
+import yaml
+import argparse
+import numpy as np
+from pathlib import Path
+from models import *
+from classifier_exp import VAEXperiment
+import torch.backends.cudnn as cudnn
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.utilities.seed import seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from dataset import VAEDataset
+from pytorch_lightning.strategies import DDPStrategy
+
+
+parser = argparse.ArgumentParser(description='Generic runner for classifier')
+parser.add_argument('--config',  '-c',
+                    dest="filename",
+                    metavar='FILE',
+                    help =  'path to the config file',
+                    default='configs/vae.yaml')
+
+args = parser.parse_args()
+with open(args.filename, 'r') as file:
+    try:
+        config = yaml.safe_load(file)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+tb_logger =  TensorBoardLogger(save_dir=config['logging_params']['save_dir'],
+                               name=config['model_params']['name'],)
+
+# For reproducibility
+seed_everything(config['exp_params']['manual_seed'], True)
+
+model = vae_models[config['model_params']['name']](**config['model_params'])
+experiment = VAEXperiment(model,
+                          config['exp_params'])
+
+data = VAEDataset(**config["data_params"], pin_memory=config['trainer_params']['gpus'])
+
+data.setup()
+runner = Trainer(logger=tb_logger,
+                 callbacks=[
+                     LearningRateMonitor(),
+                     ModelCheckpoint(save_top_k=2, 
+                                     dirpath =os.path.join(tb_logger.log_dir , "checkpoints"), 
+                                     monitor= "val_loss",
+                                     save_last= True),
+                 ],
+                 strategy=DDPStrategy(find_unused_parameters=False),
+                 **config['trainer_params'])
+
+
+
+print(f"======= Training {config['model_params']['name']} =======")
+runner.fit(experiment, datamodule=data)
